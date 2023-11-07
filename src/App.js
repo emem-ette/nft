f8a97f65066d68dd71aa323c33d797672a594a19
K_H_L_C <elizabethudoessien03@gmail.com>
2023-11-07 15:55:04 -0800

    fm

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+    const paragraph = 'Our Equilibrium collection promotes balance and calm';
+    const eth = '0.041ETH'
+    const days = '3 days left'
+
+
+    return (
+      <div className="App">
+        <div className='container'>
+           <div className='content-container'>
+  
+            <div className='flex-container'>
+              <div className='box-1'>
+                <img src='./imgs/image-equilibrium.jpg' alt='bigEquilibrium' style={{
+                  width: 300,
+                  height: 300, objectFit: 'contain', borderRadius: 20
+                }} />
+              </div>
+  
+              <div className='box-2'>
+                <div className='box-2-content-container'>
+                  <h1>equilibrium #3429</h1>
+                  <p>{paragraph}</p>
+                  <div className='flex-container-2'>
+                    <div className='lil-box-1'>
+                      <div className='icon'>
+                        <img src='./imgs/icon-ethereum.svg' alt='iconEth' className='icon' />
+                      </div>
+                      <div className='eth'>
+                        <p>{eth}</p>
+                      </div>
+                    </div>
+                    <div className='lil-box-2'>
+                      <div className='icon'>
+                        <img src='./imgs/icon-clock.svg' alt='iconClock' className='icon' />
+                      </div>
+                      <div className='days'>
+                        <p>{days}</p>
+                      </div>
+                    </div>
+  
+                  </div>
+                  <div className='hr-line'>
+                    <hr className='line' />
+                  </div>
+                  <div className='flex-container-3'>
+                    <div className='lil-box-3'>
+                      <img src='./imgs/image-avatar.png' alt='userImg' style={{ width: 50 }} className='avatarImg' />
+                    </div>
+                    <div className='lil-box-4'>
+                      <h3><span className='diffColor'>Creation of</span> Jules Wyvern</h3>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+  
+  
+          </div>
+        </div>
+      </div>
   );
 }
 
